e60ba6dc4fff0360de67c8cd7c6f2eb31ae21e2d
puru <must1080@naver.com>
2025-01-08 19:02:24 +0900

    tistory, velog 크롤링 기능 구현

---
+++ app/main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
+from fastapi.responses import JSONResponse
+import feedparser
+app = FastAPI()
+
+@app.get("/blog/tistory/{user_id}")
+async def tistory(user_id: str):
+    url = f'https://{user_id}.tistory.com/rss'
+    try:
+        rss = feedparser.parse(url)
+        entries = rss["entries"]
+        blog_contents = {'contents': []}
+        for entry in entries:
+            temp_dict = {}
+            temp_dict['title'] = entry["title"]
+            temp_dict['link'] = entry['link']
+            temp_dict['description'] = entry['description']
+            temp_dict['published'] = entry['published']
+            blog_contents['contents'].append(temp_dict)
+
+        return JSONResponse(content=blog_contents)
+
+    except requests.exceptions.RequestException as e:
+        # 요청 중 오류 발생 시 예외 처리
+        print(f'An error occurred: {e}')
+        return {'error': f'An error occurred: {e}'}
+
+
+@app.get("/blog/velog/{user_id}")
+async def velog(user_id: str):
+    url = f'https://v2.velog.io/rss/{user_id}'
+    try:
+        rss = feedparser.parse(url)
+        entries = rss["entries"]
+        blog_contents = {'contents': []}
+        for entry in entries:
+            temp_dict = {}
+            temp_dict['title'] = entry["title"]
+            temp_dict['link'] = entry['link']
+            temp_dict['description'] = entry['description']
+            temp_dict['published'] = entry['published']
+            blog_contents['contents'].append(temp_dict)
+
+        return JSONResponse(content=blog_contents)
+
+    except requests.exceptions.RequestException as e:
+        # 요청 중 오류 발생 시 예외 처리
+        print(f'An error occurred: {e}')
+        return {'error': f'An error occurred: {e}'}
